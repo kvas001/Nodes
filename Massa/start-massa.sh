ef3c4fc43127817d1ec8cbf6419f7e64191639d7
kvas001 <95753493+kvas001@users.noreply.github.com>
2024-01-15 20:50:20 +0500

    Update start-massa.sh

--- Massa/start-massa.sh
+++ Massa/start-massa.sh
@@ -94,3 +94,10 @@ balance=$(./massa-client wallet_info -p $pass | grep "Balance:" | awk '{ print $
 int_balance=${balance%%.*}
 date	
 echo Баланс = $int_balance
+if [[ "$int_balance" -gt "100" ]] ; then
+			echo "More than 100. Баланс токенов более 100. "
+			resp=$(./massa-client buy_rolls $my_wallet_addr 1 0 -p $pass)
+			echo $resp
+	elif [[ "$int_balance" -lt "100" ]] ; then
+			echo "Less than 100. Баланс токенов менее 100."
+fi
